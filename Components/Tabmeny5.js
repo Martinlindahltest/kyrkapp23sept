7eac3312399812d8b31e5e25cb086d5969fdf3d9
Martinlindahltest <basmartin@gmail.com>
2019-09-30 20:43:15 +0200

    Kalenderbilden har rätt storlek

---
+++ Components/Tabmeny5.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+export default function Tabmeny5() {
+
+    const styles = StyleSheet.create({
+        img: {
+          height: 25,
+          width: '100%',
+          resizeMode: "contain"
+        },
+        box: {
+            
+        }
+
+      });
+
+    return (
+        <View>
+            <View><Text>1</Text></View>
+
+            <View><Text>2</Text></View>
+
+            <View><Text>3</Text></View>
+
+            <View><Text>4</Text></View>
+
+            <View><Text>5</Text></View>
+
+        </View>
+    )
+}
