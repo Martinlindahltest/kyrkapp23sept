b1e2d4b6316c79c763b915117fc527d7759fbe91
Martinlindahltest <basmartin@gmail.com>
2019-10-03 23:30:32 +0200

    Flexbox fungerar på anslagstavlan

--- Screens/AnslagstavlaScreen.js
+++ Screens/AnslagstavlaScreen.js
@@ -1,24 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { DrawerActions } from 'react-navigation-drawer';
 
 import HeaderButton from '../Components/HeaderButton'
 
 
+const styles = StyleSheet.create({
+    col:{
+        flex: 1,
+         height: '33%',
+    },
+  });
+
+
 const AnslagstavlaScreen = () => {
     return (
-
-        <View >
-            <Text>AnslagstavlaScreen Martins projekt</Text>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+      
+        <View style={{...styles.col, backgroundColor: 'powderblue'}} >            
         </View>
+
+        <View style={{...styles.col, backgroundColor: 'skyblue'}} >
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 1, flexDirection: 'column'}}><Text>1ttan</Text></View>
+                <View style={{flex: 1, flexDirection: 'column'}}><Text>1ttan</Text></View>
+            </View>
+        </View>
+
+        <View style={{...styles.col , height: '34%', backgroundColor: 'steelblue'}} >            
+        </View>
+      </View>
 
     ) 
 }
 
 AnslagstavlaScreen.navigationOptions = navData => {
     return {
-      headerTitle: 'Händelser',
+      headerTitle: 'Anslagstavla',
       headerLeft: (
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
           <Item
