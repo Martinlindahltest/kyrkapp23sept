c460e608bd86c6852ada58facacb9deea7cc05ce
Martinlindahltest <basmartin@gmail.com>
2019-10-11 12:08:37 +0200

    Rubriken från wp sysn i fetchsenastehändelse

--- Components/FetchSenasteHandelse.js
+++ Components/FetchSenasteHandelse.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+
 
 export default class FetchSenasteHandelse extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            WpJson: {}
+            WpJson: {},
+            isLoaded: false
         }
 
     }
 
     componentDidMount() {
 
-        const jsonURL = 'https://tullinge.digitalindahl.com/wp-json/wp/v2/posts'
+        const jsonURL = 'https://tullinge.digitalindahl.com/wp-json/wp/v2/posts?posts?per_page=1'
         fetch(jsonURL)
         .then(response => {
           return response.json();
@@ -22,18 +24,31 @@ export default class FetchSenasteHandelse extends Component {
          //   console.log('setstate')
 
           this.setState({
-            WpJson: WpJson
+            WpJson: WpJson,
+            isLoaded: true
           })
         });
     }
 
     render() {
-        return (
+
+      if(this.state.isLoaded == true){
+          //console.log('wp console.log', this.state.WpJson[0].title.rendered)
+          return (
             <View>
-                <Text>{
-                  //  console.log('wp title', this.state)
-                    }</Text>
-            </View>
-        )
+                <Text >{this.state.WpJson[0].title.rendered}</Text>
+            </View>)
+      }
+      else
+      {
+        return (
+          <View>
+              <Text>
+              </Text>
+          </View>
+      )
+
+      }
+
     }
 }
