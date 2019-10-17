02ee4b66a2cfe64328fa59bbd12a2279b6a6197a
Martinlindahltest <basmartin@gmail.com>
2019-10-17 09:01:28 +0200

    la till text-knapp på api och verksamhet detalj

--- Screens/VerksamhetDetaljScreen.js
+++ Screens/VerksamhetDetaljScreen.js
@@ -1,20 +1,37 @@
 import React from 'react'
-import { Text, ScrollView } from 'react-native'
+import { Text, ScrollView, Button, Linking } from 'react-native'
 
 
 
 const VerksamhetDetaljScreen = (props) => {
 
 
-    const {
+    let {
         rubrik, 
         typ, 
         beskrivning,
+        text_pa_knapp_1,
         url1,
+        text_pa_knapp_2,
         url2,
         dag,
         tillpopular
     } = props.navigation.state.params.Aktivitet
+
+    console.log('props.navigation.state.params', props.navigation.state.params)
+
+    let knapp1 = []
+    let knapp2 = []
+
+    if(url1){
+        //url1 = url1.toString()
+        knapp1.push(<Button title={text_pa_knapp_1} onPress={ ()=>{ Linking.openURL(url1)}} />)
+    }
+
+    if(url2){
+        //url1 = url1.toString()
+        knapp2.push(<Button title={text_pa_knapp_2} onPress={ ()=>{ Linking.openURL(url2)}} />)
+    }
 
     return (
     
@@ -22,7 +39,8 @@ const VerksamhetDetaljScreen = (props) => {
             <Text>VerksamhetDetaljScreen</Text>
             <Text>{rubrik}  </Text>
             <Text>{beskrivning}  </Text>
-
+            {knapp1}
+            {knapp2}
 
         </ScrollView>
         
