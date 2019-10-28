f91d2902093e1672d4a51e897584834e950d2c72
Martinlindahltest <basmartin@gmail.com>
2019-10-28 14:43:23 +0100

    text på bilder kalender

--- Components/HeaderImage.js
+++ Components/HeaderImage.js
@@ -5,6 +5,17 @@ export default function HeaderImage(props) {
 
     const styles = StyleSheet.create({
         img: {width: '100%', height: 165, resizeMode: 'cover'},
+        rubriktext: {
+          fontSize: 30,
+          color: 'white',
+          backgroundColor:'black',
+          padding: 10
+        },
+        rubriktextView: {
+          flex: 1,
+          justifyContent: 'center', 
+          alignItems: 'center' 
+        }
         
       });
 
@@ -18,25 +29,40 @@ export default function HeaderImage(props) {
 
       let component = 
         <ImageBackground source={require('../assets/Kalender17okt/kal-verk-front.jpg')} style={styles.img}>
+          <View style={styles.rubriktextView}>
+            <Text style={styles.rubriktext}>Kalender</Text>
+          </View>
         </ImageBackground>
 
         if(props.screen == 'Gudtjänst'){
           component = 
           <ImageBackground source={require('../assets/Kalender17okt/kal-verk-gtj.jpg')} style={styles.img}>
+                      <View style={styles.rubriktextView}>
+                         <Text style={styles.rubriktext}>{props.screen}</Text>
+                      </View>
           </ImageBackground>        
           }
           if(props.screen == 'Musik'){
             component = 
             <ImageBackground source={require('../assets/Kalender17okt/kal-verk-musik.jpg')} style={styles.img}>
+                      <View style={styles.rubriktextView}>
+                         <Text style={styles.rubriktext}>{props.screen}</Text>
+                      </View>
             </ImageBackground>        
             }
             if(props.screen == 'Barn'){
               component = 
               <ImageBackground source={require('../assets/Kalender17okt/kal-verk-barn.jpg')} style={styles.img}>
+                      <View style={styles.rubriktextView}>
+                         <Text style={styles.rubriktext}>{props.screen}</Text>
+                      </View>
               </ImageBackground>        }
             if(props.screen == 'Ung/Vuxen'){
               component = 
               <ImageBackground source={require('../assets/Kalender17okt/kal-verk-ungVux.jpg')} style={styles.img}>
+                      <View style={styles.rubriktextView}>
+                         <Text style={styles.rubriktext}>{props.screen}</Text>
+                      </View>
               </ImageBackground>        }
 
 
