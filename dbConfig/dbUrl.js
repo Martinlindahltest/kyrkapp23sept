4670d3bed4d3c39173817c8d8ba2ac544d644e31
Martinlindahltest <basmartin@gmail.com>
2019-11-03 17:43:51 +0100

    Tillbaka till s3

--- dbConfig/dbUrl.js
+++ dbConfig/dbUrl.js
@@ -1,5 +1,5 @@
-//const dbURL = 'https://jsontestmartin.s3.eu-north-1.amazonaws.com/tullinge30sept.json'
+const dbURL = 'https://jsontestmartin.s3.eu-north-1.amazonaws.com/tullinge30sept.json'
 
-const dbURL = 'http://35.158.198.4:4000/api/kalender'
+//const dbURL = 'http://35.158.198.4:4000/api/kalender'
 
 module.exports = dbURL;
