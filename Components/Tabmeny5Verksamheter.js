e9e141d821803af392a26594de16b33e3ba164c5
Martinlindahltest <basmartin@gmail.com>
2019-11-11 09:52:29 +0100

    tab5verksamheter visas bra men leder till kalender

---
+++ Components/Tabmeny5Verksamheter.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet, Button, TouchableOpacity, ImageBackground } from 'react-native'
+
+export default function Tabmeny5(props) {
+
+  const styles = StyleSheet.create({
+    box:{
+        flex: 1,
+        backgroundColor: 'black'
+
+    },
+    img: {
+      height:'100%',
+      width:'100%',
+      resizeMode: "contain"
+    }, 
+    textUnderBild: {
+      color: 'red'
+    }
+
+  });
+
+    return (
+
+
+      <View style={{flex: 1, flexDirection: 'row', height:70}}>
+
+<View style={styles.box}>
+      <TouchableOpacity onPress={()=> props.KalenderScreenProps.navigation.navigate('Kalender')}>
+        <Image 
+        style={styles.img}
+        source={require('../assets/tab5/Alla.png')}
+        /> 
+      </TouchableOpacity>
+    </View>
+
+<View style={styles.box}>
+      <TouchableOpacity onPress={()=> props.KalenderScreenProps.navigation.navigate('KalenderGtj')}>
+        <Image 
+        style={styles.img}
+        source={require('../assets/tab5/Gudstjanst.png')}
+        /> 
+      </TouchableOpacity>
+    </View>
+
+    <View style={styles.box}>
+      <TouchableOpacity onPress={()=> props.KalenderScreenProps.navigation.navigate('KalenderMusik')}>
+        <Image 
+        style={styles.img}
+        source={require('../assets/tab5/Musik.png')}
+        /> 
+      </TouchableOpacity>
+    </View>
+
+    <View style={styles.box}>
+      <TouchableOpacity onPress={()=> props.KalenderScreenProps.navigation.navigate('KalenderBarn')}>
+        <Image 
+        style={styles.img}
+        source={require('../assets/tab5/Barn.png')}
+        /> 
+      </TouchableOpacity>
+    </View>
+
+
+
+    <View style={styles.box}>
+      <TouchableOpacity onPress={()=> props.KalenderScreenProps.navigation.navigate('KalenderVuxen')}>
+        <Image 
+        style={styles.img}
+        source={require('../assets/tab5/Vuxen.png')}
+        /> 
+      </TouchableOpacity>
+    </View>
+
+
+
+
+
+    </View>
+    )
+}
--- Screens/VerksamheterScreen.js
+++ Screens/VerksamheterScreen.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { ScrollView, View, Text } from 'react-native'
+import { ScrollView, View, Text, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { DrawerActions } from 'react-navigation-drawer';
 import FetchVerksamheter from '../Components/FetchVerksamheter'
 import { WebView } from 'react-native-webview';
+import Tabmeny5Verksamheter from '../Components/Tabmeny5Verksamheter'
+import TabmenyText from '../Components/TabmenyText'
+
 
 
 import HeaderButton from '../Components/HeaderButton'
@@ -12,14 +15,34 @@ import HeaderButton from '../Components/HeaderButton'
 
 
 const VerksamheterScreen = (props) => {
+
+  const styles = StyleSheet.create({
+    tabContainer: {
+      height: 70 
+    },
+    textContainer: {
+      height: 15,
+    }
+  });
     return (
       /*
                   <WebView source={{ uri: 'https://tullinge.digitalindahl.com/verksamheter' }} />
 
       */
+     <View>
+        <View>
+          <View style={styles.tabContainer}>
+          <Tabmeny5Verksamheter KalenderScreenProps={props} />
+          </View>
+          <View style={styles.textContainer}>
+          <TabmenyText />
+          </View>
+        </View>
+     
      <ScrollView>
                <FetchVerksamheter navigateReferens={props} />
      </ScrollView>
+     </View>
     )
 } 
 
