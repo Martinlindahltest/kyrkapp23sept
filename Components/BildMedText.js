72b81952877e02bf64a1d42ea0cb52245151604d
Martinlindahltest <basmartin@gmail.com>
2019-11-13 17:55:52 +0100

    Innan expo publish

--- Components/BildMedText.js
+++ Components/BildMedText.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native'
 
-
+//innan expo publish
 
 
 const BildMedLank = (props) => {
