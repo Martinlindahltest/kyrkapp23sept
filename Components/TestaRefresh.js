0479a4a358c6a818300370a9cd274eb636ccb6ae
Martinlindahltest <basmartin@gmail.com>
2019-11-18 11:16:54 +0100

    Ändrad URL till Testdatabas

---
+++ Components/TestaRefresh.js
@@ -0,0 +1,33 @@
+import { ScrollView, RefreshControl } from 'react-native';
+import { Component } from 'react'
+
+
+
+class TestaRefresh extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+    };
+  }
+
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    fetchData().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
+
+  render() {
+    return (
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }
+      />
+    );
+  }
+}
