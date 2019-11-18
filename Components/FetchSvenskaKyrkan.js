3e3b82dcfac3917e21f7709372a1b63647ce13f8
Martinlindahltest <basmartin@gmail.com>
2019-11-18 19:37:59 +0100

    fetch från svenskakyrkan fungerar class component

--- Components/FetchSvenskaKyrkan.js
+++ Components/FetchSvenskaKyrkan.js
@@ -1,31 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import { Text, View } from 'react-native'
+
+export default class FetchSvenskaKyrkan extends Component {
+  state = {
+    hasErrors: false,
+    planets: {},
+    isloading: true
+  };
+
+  componentDidMount() {
+    fetch("https://api.svenskakyrkan.se/calendarsearch/v4/SearchByParent?orgId=7681&$top=1&apikey=b085d57a-4f3d-4742-933f-ce94a840c203")
+      .then(res => res.json())
+      .then(res => this.setState({ planets: res, isloading: false }))
+      .catch(() => this.setState({ hasErrors: true }));
+  }
 
 
-const FetchSvenskaKyrkan = () => {
-    const [hasError, setErrors] = useState(false);
-    const [planets, setPlanets] = useState({});
-  
-    async function fetchData() {
-      const res = await fetch("https://api.svenskakyrkan.se/calendarsearch/v4/SearchByParent?orgId=7681&apikey=b085d57a-4f3d-4742-933f-ce94a840c203");
-      res
-        .json()
-        .then(res => setPlanets(res))
-        .catch(err => setErrors(err));
+
+
+  render() {
+
+    let variabelnamn = 'lalala'
+
+    if(this.state.isloading === false){
+      variabelnamn = this.state.planets.value[0].Active
+      console.log('variabelnamn', variabelnamn)
     }
-  
-    useEffect(() => {
-      fetchData();
-    });
 
-  return(
+    return(
       <View>
-<Text>
-    {JSON.stringify(planets)}
-</Text>
-</View>
-
-    ) 
-
-};
-export default FetchSvenskaKyrkan;
+        <Text>
+          lalala
+        </Text>
+      </View>
+    )
+  }
+}
