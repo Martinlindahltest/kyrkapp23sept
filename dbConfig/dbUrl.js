fa8cc45d0bf1f257a819d5720d76172bc89ae5db
Martinlindahltest <basmartin@gmail.com>
2019-11-24 17:37:14 +0100

    apiGateway som db url

--- dbConfig/dbUrl.js
+++ dbConfig/dbUrl.js
@@ -1,7 +1,8 @@
 //const dbURL = 'https://jsontestmartin.s3.eu-north-1.amazonaws.com/tullinge30sept.json'
 
 //const dbURL = 'http://35.158.216.149:4000/api/kalender'
-const dbURL = 'http://3.120.247.104:4000/'
+//const dbURL = 'http://3.120.247.104:4000/'
+const dbURL = 'https://uk934jff46.execute-api.eu-central-1.amazonaws.com/forsta'
 
   
 module.exports = dbURL;
