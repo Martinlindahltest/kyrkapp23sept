e2a83ac308d91d8ad627bde13ca1c5f9fff8e718
Martinlindahltest <basmartin@gmail.com>
2019-12-30 22:12:53 +0100

    martins backend fungerar

--- Components/FetchApp.js
+++ Components/FetchApp.js
@@ -57,35 +57,28 @@ class FetchApp extends Component {
     
 
     render() {
+
+        let arrayMedDatabasobjekt = []
+
+
         if(this.state.isLoading == false){
 
-        let arrayMedDatabasobjekt = this.state.tullingeJson.value.map(obj => {
-            return {
-                Aktivitet: "Adventsgudstjänst",
-                Dag: "Söndag",
-                Datum: obj.StartTime,
-                Internnotering: "Fackeltåg från Tullinge gård.",
-                Lokal: "Tullinge kyrka",
-                Musiker: null,
-                Personal: null,
-                Pr_st: "Eva Ajaxén & Peter Johansson",
-                StartSlut: "16.00-17.00",
-                Vaktm_stare: null,
-                Verksamhetstyp: "Gtj",
-                starttid: "16.00",
-                uuid: "5cbded96-bb66-4fd3-9679-0b8e284ea9e0"
-            }
-        })
+        arrayMedDatabasobjekt = this.state.tullingeJson
+      
 
-        console.log(arrayMedDatabasobjekt)
-        //let dagensDatum =new Date().toISOString() 
+
+    
+
+    }
+
+            //let dagensDatum =new Date().toISOString() 
         //dagensDatum är nu gårdagens datum
         var d = new Date();
         d.setDate(d.getDate() - 1);
         let dagensDatum = d.toISOString()
-    
 
-    }
+    console.log(arrayMedDatabasobjekt)
+
         let jsxArray	 
 
         if(this.state.tullingeJson.length > 0){
--- dbConfig/dbUrl.js
+++ dbConfig/dbUrl.js
@@ -2,10 +2,10 @@
 
 //const dbURL = 'http://35.158.216.149:4000/api/kalender'
 //const dbURL = 'http://3.120.247.104:4000/'
-// const dbURL = 'https://uk934jff46.execute-api.eu-central-1.amazonaws.com/forsta'
+ const dbURL = 'https://uk934jff46.execute-api.eu-central-1.amazonaws.com/forsta'
 
 //svenska kyrkan Martin Lindahl
-const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchByParent?orgId=7681&apikey=b085d57a-4f3d-4742-933f-ce94a840c203	'
+//const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchByParent?orgId=7681&apikey=b085d57a-4f3d-4742-933f-ce94a840c203	'
 
   
 module.exports = dbURL;
