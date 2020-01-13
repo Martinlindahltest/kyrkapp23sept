c3b819d35815c9919b98559bac361ecb46cd6d86
Martinlindahltest <basmartin@gmail.com>
2020-01-13 12:56:11 +0100

    api ändrat till  beebyte variabel.digitalindahl

--- dbConfig/dbUrl.js
+++ dbConfig/dbUrl.js
@@ -2,7 +2,9 @@
 
 //const dbURL = 'http://35.158.216.149:4000/api/kalender'
 //const dbURL = 'http://3.120.247.104:4000/'
-const dbURL = 'https://uk934jff46.execute-api.eu-central-1.amazonaws.com/forsta'
+//const dbURL = 'https://uk934jff46.execute-api.eu-central-1.amazonaws.com/forsta'
+
+const dbURL = 'https://variabel.digitalindahl.com/'
 
   
 module.exports = dbURL;
