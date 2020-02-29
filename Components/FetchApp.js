cf8c71e1430edc33f9b4f227ed7616e99e523497
Martinlindahltest <basmartin@gmail.com>
2020-02-29 17:06:57 +0100

    Verksamhetstyp = verksamhetskoder kalender

--- Components/FetchApp.js
+++ Components/FetchApp.js
@@ -23,80 +23,86 @@ class FetchApp extends Component {
         const jsonURL = dbURL
 
         fetch(jsonURL)
-        .then(response => {
-          return response.json();
-        })
-        .then(tullingeJson => {
+            .then(response => {
+                return response.json();
+            })
+            .then(tullingeJson => {
 
 
-           //Sorterar i datumordning
-           let arrayMedDatabasobjekt = tullingeJson.value.sort(function(a, b) {
-        return (a.StartTime < b.StartTime) ? -1 : ((a.StartTime > b.StartTime) ? 1 : 0);
-    });
+                //Sorterar i datumordning
+                let arrayMedDatabasobjekt = tullingeJson.value.sort(function (a, b) {
+                    return (a.StartTime < b.StartTime) ? -1 : ((a.StartTime > b.StartTime) ? 1 : 0);
+                });
 
-    const regex = /<[^>]*>/g
-
-    
-    function strReplace(text) {
-        let tvattadText = null
-        if (text != null){
-            tvattadText = text.replace(regex, " ")
-        }
-        return tvattadText
-    }
-
-    function siffratilldag(siffra) {
-        let dagnamn = ['sön', 'mån', 'tis', 'ons', 'tor', 'fre', 'lör']
-        return dagnamn[siffra]
-    }
-
-       //Skapar data som liknar den gamla
-        arrayMedDatabasobjekt = arrayMedDatabasobjekt.map(obj => {
-
-            return {
-            Aktivitet: obj.Title,
-            Datum: obj.StartTime,
-            Dag: siffratilldag(new Date(obj.StartTime).getDay()),
-            Internnotering: strReplace(obj.Description),
-            Lokal: obj.Place.Name,
-            Musiker: null,
-            Personal: null,
-            Pr_st: null,
-            StartSlut: obj.StartTime,
-            Vaktm_stare: null,
-            Verksamhetstyp: obj.CalendarSubGroups,
-            starttid: obj.StartTime.slice(11, 16),
-            uuid: obj.Id,
-
-        }
+                const regex = /<[^>]*>/g
 
 
-    })
+                function strReplace(text) {
+                    let tvattadText = null
+                    if (text != null) {
+                        tvattadText = text.replace(regex, " ")
+                    }
+                    return tvattadText
+                }
 
-    console.log('arrayMedDatabasobjekt', arrayMedDatabasobjekt)
+                function siffratilldag(siffra) {
+                    let dagnamn = ['sön', 'mån', 'tis', 'ons', 'tor', 'fre', 'lör']
+                    return dagnamn[siffra]
+                }
 
-    if(this.props.KategoriFilter === 'Gtj'){
-        arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Gudstjänst & mässa');
-   }
-   if(this.props.KategoriFilter === 'Barn'){
-    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Barn');
-  }
-  if(this.props.KategoriFilter === 'Vuxen'){
-    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Vuxen');
-  }
-   if(this.props.KategoriFilter === 'Musik'){
-    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Musik');
+                //Skapar data som liknar den gamla
+                arrayMedDatabasobjekt = arrayMedDatabasobjekt.map(obj => {
+                    let verksamhetskoder = obj.CalendarSubGroups.map(objIArray => {
+                        return objIArray.Id
+                    })
 
-}
+                    console.log(obj.Title, 'kod:', verksamhetskoder)
+
+                    return {
+                        Aktivitet: obj.Title,
+                        Datum: obj.StartTime,
+                        Dag: siffratilldag(new Date(obj.StartTime).getDay()),
+                        Internnotering: strReplace(obj.Description),
+                        Lokal: obj.Place.Name,
+                        Musiker: null,
+                        Personal: null,
+                        Pr_st: null,
+                        StartSlut: obj.StartTime,
+                        Vaktm_stare: null,
+                        Verksamhetstyp: verksamhetskoder,
+                        starttid: obj.StartTime.slice(11, 16),
+                        uuid: obj.Id,
+
+
+                    }
+
+
+                })
+
+                //console.log('arrayMedDatabasobjekt', arrayMedDatabasobjekt)
+
+                if (this.props.KategoriFilter === 'Gtj') {
+                    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Gudstjänst & mässa');
+                }
+                if (this.props.KategoriFilter === 'Barn') {
+                    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Barn');
+                }
+                if (this.props.KategoriFilter === 'Vuxen') {
+                    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Vuxen');
+                }
+                if (this.props.KategoriFilter === 'Musik') {
+                    arrayMedDatabasobjekt = arrayMedDatabasobjekt.filter(obj => obj.Verksamhetstyp === 'Musik');
+
+                }
 
 
 
-       // console.log(arrayMedDatabasobjekt)
-          this.setState({
-            tullingeJson: arrayMedDatabasobjekt,
-            isLoading: false
-          })
-        });
+                // console.log(arrayMedDatabasobjekt)
+                this.setState({
+                    tullingeJson: arrayMedDatabasobjekt,
+                    isLoading: false
+                })
+            });
     }
 
 
@@ -109,16 +115,16 @@ class FetchApp extends Component {
         let dagensDatum = d.toISOString()
 
 
-        let jsxArray	 
+        let jsxArray
 
-        if(this.state.isLoading == false){
+        if (this.state.isLoading == false) {
             jsxArray = this.state.tullingeJson.map(obj => {
-                if(obj.Datum >= dagensDatum || obj.Datum == dagensDatum){
+                if (obj.Datum >= dagensDatum || obj.Datum == dagensDatum) {
                     let splitDatum = obj.Datum.split("T")[0]
                     let splitDatum2 = splitDatum.split('-')
 
-               return <Display key={obj.uuid} data={obj} navRef={this.props.navigateReferens} objektetsMånad={splitDatum2[1]} objektetsDag={splitDatum2[2]}   />
-            }
+                    return <Display key={obj.uuid} data={obj} navRef={this.props.navigateReferens} objektetsMånad={splitDatum2[1]} objektetsDag={splitDatum2[2]} />
+                }
 
             })
 
@@ -126,8 +132,8 @@ class FetchApp extends Component {
 
         return (
             <View>
-                                {jsxArray}
-                                <Spacer hojd={100} />
+                {jsxArray}
+                <Spacer hojd={100} />
 
 
             </View>
