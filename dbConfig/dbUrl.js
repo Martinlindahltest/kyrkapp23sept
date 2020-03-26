38d234d787b515adb17772225a29e0c348f3ee30
Martinlindahltest <basmartin@gmail.com>
2020-03-26 19:00:31 +0100

    hårdkodad api string fungerar

--- dbConfig/dbUrl.js
+++ dbConfig/dbUrl.js
@@ -8,7 +8,7 @@
 //const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchByParent?orgId=7681&apikey=b085d57a-4f3d-4742-933f-ce94a840c203	'
 
 //svenska kyrkan Martin Lindahl filtrerad på long-lat
-const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchBySpatial()?longitude=17.882806&latitude=59.212262&radius=3000&$top=100&apikey=b085d57a-4f3d-4742-933f-ce94a840c203'
+//const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchBySpatial()?longitude=17.882806&latitude=59.212262&radius=3000&$top=100&apikey=b085d57a-4f3d-4742-933f-ce94a840c203'
+const dbURL = 'https://api.svenskakyrkan.se/calendarsearch/v4/SearchBySpatial()?longitude=17.882806&latitude=59.212262&radius=3000&apikey=b085d57a-4f3d-4742-933f-ce94a840c203&$orderby=StartTime asc&$filter=Deleted eq false and StartTime gt 2020-03-26&$top=100'
 
-  
 module.exports = dbURL;
