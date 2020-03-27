5b8f777dfee9fa842bace409b237468d2204d614
Martinlindahltest <basmartin@gmail.com>
2020-03-27 16:33:56 +0100

    Nu verkar det fungera med sdk 35

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import MealsNavigator from './Navigation/MealsNavigator'
-import * as Font from 'expo-font'
+import * as Font from 'expo-font';
 import { AppLoading } from 'expo'
+import { Assets } from 'react-navigation-stack';
 
 
 const fetchFonts = () => {
   return Font.loadAsync({
     'avenir-roman': require('./assets/fonts/Avenir-Roman.ttf')
   });
+  
 }
 
 export default function App() {
